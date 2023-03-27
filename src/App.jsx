b5b92e8b553e454dbf12cc4b2fc90c18b994c7a7
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-03-27 10:22:48 +0800

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Die from './components/Die'
+
+const App = () => {
+  return (
+    <main>
+      <div className='dice-container'>
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Die.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Die = (props) => {
+  return (
+    <div className="die-face">
+        <h2>{props.value}</h2>
+    </div>
+  )
+}
+
+export default Die
